4be05b792e1015527776acf8b79098ba97156b11
Ademola <gbengaadepoju75@gmail.com>
2024-06-23 11:28:46 -0500

    editted routing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-import dynamic from "next/dynamic";
 import PrivateRoute from "../component/PrivateRoute";
 import Logout from "../component/Logout";
+import dynamic from "next/dynamic";
 
 const DashboardPage = dynamic(() => import("./dash"), {
   ssr: false,
